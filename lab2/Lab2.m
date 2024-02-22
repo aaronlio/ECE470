6fd770e5b25e14dc47f70703d2f98b1ef1cff843
SAKURA <78051522+Joshua1030@users.noreply.github.com>
2024-02-21 21:19:51 -0500

    modify forward, inverse, and mykuka for lab2

--- lab2/Lab2.m
+++ lab2/Lab2.m
@@ -1,11 +1,5 @@
-%% MyPuma Testing
+%% MyKuka Testing
 
-DH = [0     76     0     pi/2;
-      0     -23.65 43.23 0;
-      0     0      0     pi/2;
-      0     43.18  0     -pi/2;
-      0     0      0     pi/2;
-      0     20     0     0];
 %     theta d      a     alpha
 DH = [0     400    25    pi/2;
       0     0      315   0;
@@ -14,10 +8,11 @@ DH = [0     400    25    pi/2;
       0     0      0     pi/2;
       0     161.44 -296.23  0];
 
-myrobot = mykukua(DH);
+kuka = mykuka(DH);
 
-ans1 = forward_kuka([pi/5 pi/3 -pi/4 pi/4 pi/3 pi/4],kuka);
-inverse_kuka(ans1,kuka);
+H1 = forward_kuka([pi/5 pi/3 -pi/4 pi/4 pi/3 pi/4],kuka);
+test_q = inverse_kuka(H1,kuka);
+plot(kuka, test_q)
 
 %% Plot a sample joint space trajectory
 theta1=linspace(0,pi,200);
